7492c87a3668551db18dc1d010a84a15abec4d6e
carlos219903 <sales@groupwj.com>
2026-09-09 15:20:59 +0200

    Fix root route

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/",
+          destination: "/index.html",
+        },
+      ],
+      afterFiles: [],
+      fallback: [],
+    };
+  },
+};
 
 export default nextConfig;
